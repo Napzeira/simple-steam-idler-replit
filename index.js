d78ddb5f35ba6d32bc7484c2a979f15624290dbf
Napzeira <112515227+Napzeira@users.noreply.github.com>
2025-03-10 21:09:49 -0500

    Update 2025 index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 550, 438100, 774171 ];  // Enter here AppIDs of the needed games
+var games = [730, 550, 438100, 1966720 ];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
